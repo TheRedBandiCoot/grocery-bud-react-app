bc7b553d12f653c5680a8109479bb5942221eb33
Gourab Chatterjee <gourabchatterjee623@gmail.com>
2023-08-16 22:49:05 +0530

    remove vite sass package

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteSass from 'vite-plugin-sass';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteSass({
-      sassOptions: {
-        outputStyle: 'compressed',
-      },
-      includePaths: ['./src'],
-    }),
-  ],
+  plugins: [react()],
   server: {
     port: 3000,
-  },
-  css: {
-    modules: true,
   },
 });
